a402c9f8ba8808a727ce684561dbc0e5ef6f91f2
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-04-28 12:23:23 +0200

    Custom auth provider added

---
+++ code/opPlanner.ApiGateway/src/main/java/opPlanner/ApiGateway/CustomAuthenticationProvider.java
@@ -0,0 +1,40 @@
+package opPlanner.ApiGateway;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Michael on 28.04.2015.
+ */
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        System.out.println("Auth user!!!");
+
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if (name.equals("admin") && password.equals("system")) {
+            List<GrantedAuthority> grantedAuths = new ArrayList<>();
+            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+            return auth;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        System.out.println("Auth: supports: " + authentication);
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
