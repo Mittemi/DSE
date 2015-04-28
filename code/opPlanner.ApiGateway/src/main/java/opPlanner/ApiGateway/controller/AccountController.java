24c639b1f1ff0e6e829422d5ed41fa54ea5f46ae
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-04-28 17:57:29 +0200

    AccountController

---
+++ code/opPlanner.ApiGateway/src/main/java/opPlanner/ApiGateway/controller/AccountController.java
@@ -0,0 +1,27 @@
+package opPlanner.ApiGateway.controller;
+
+import opPlanner.ApiGateway.AuthResult;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Michael on 28.04.2015.
+ */
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @RequestMapping(value = "/details", method = RequestMethod.GET, produces = "application/json")
+    public AuthResult details() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        List<String> roles = auth.getAuthorities().stream().map(x -> x.getAuthority()).collect(Collectors.toList());
+        return new AuthResult(auth.isAuthenticated(), roles.toArray(new String[roles.size()]));
+    }
+}
