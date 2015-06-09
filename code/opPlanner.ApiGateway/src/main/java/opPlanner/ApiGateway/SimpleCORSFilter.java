f0e2960cd89ee78d5f75a1ed152059aba7071173
Lukas Kraenkl <studium@kraenkl.com>
2015-06-09 16:43:53 +0200

    Allow PUT Method for creating

--- code/opPlanner.ApiGateway/src/main/java/opPlanner/ApiGateway/SimpleCORSFilter.java
+++ code/opPlanner.ApiGateway/src/main/java/opPlanner/ApiGateway/SimpleCORSFilter.java
@@ -22,7 +22,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials: ", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",/* "x-requested-with, Authorization"*/ "Origin,Content-Type,Accept,Authorization");
         chain.doFilter(req, res);
