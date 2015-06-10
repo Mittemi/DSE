f4cfb77c94266fe8efb82579927aacb8c75e33aa
Lukas Kraenkl <studium@kraenkl.com>
2015-06-10 12:18:49 +0200

    Changes main ui design

--- code/opPlanner.WebUI/app/slot/slot.js
+++ code/opPlanner.WebUI/app/slot/slot.js
@@ -4,29 +4,12 @@ angular.module('myApp.slot', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
-
       .when('/listslot', {
-        templateUrl: 'slot/views/listslot.html',
+        templateUrl: 'slot/listslot.html',
         controller: 'listSlotCtrl'
-      })
-      .when('/addslot', {
-        templateUrl: 'slot/views/addslot.html',
-        controller: 'addSlotCtrl'
-      })
-      .when('/removeslot', {
-        templateUrl: 'slot/views/removeslot.html',
-        controller: 'removeSlotCtrl'
       })
 }])
 
 .controller('listSlotCtrl', [function() {
 
 }])
-
-    .controller('addSlotCtrl', [function() {
-
-    }])
-
-    .controller('removeSlotCtrl', [function() {
-
-    }]);
