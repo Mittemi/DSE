d92ed439d7b7cbd94a5c047348c9a8616a54b83f
Lukas Kraenkl <studium@kraenkl.com>
2015-06-10 09:14:25 +0200

    TODO init db script

---
+++ docker/initdb.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+## TODO
+## Beispielscript fuer DB inserts
+
+gosu postgres postgres --single <<- EOSQL
+	CREATE USER klinisys WITH PASSWORD 'secret';
+	CREATE DATABASE klinisys;
+	GRANT ALL PRIVILEGES ON DATABASE klinisys TO klinisys;
+EOSQL
