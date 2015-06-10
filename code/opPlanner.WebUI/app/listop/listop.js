6bfb63c34a5dd0312f93e04b632cddd745bb0629
Lukas Kraenkl <studium@kraenkl.com>
2015-06-10 16:39:58 +0200

    Set comments

--- code/opPlanner.WebUI/app/listop/listop.js
+++ code/opPlanner.WebUI/app/listop/listop.js
@@ -37,8 +37,6 @@ angular.module('myApp.listop', ['ngRoute'])
                   });
           };
 
-
-
           /**
            * Supress the build-in sorting in ng-repeat, JSON format is preserved
            * @param obj
@@ -51,34 +49,58 @@ angular.module('myApp.listop', ['ngRoute'])
               return Object.keys(obj);
           };
 
+          /**
+           * Sets the filter on the type
+           * @param type
+           */
           $scope.setSelectedType = function(type){
               $scope.selectedType = type;
               $scope.openType = null;
           };
 
+          /**
+           * Sets the filter on the hospital
+           * @param khname
+           */
           $scope.setSelectedKH = function(khname){
                 $scope.selectedKH = khname;
               $scope.openKH = null;
           };
 
-
+          /**
+           * Sets the filter on the doctor
+           * @param docname
+           */
           $scope.setSelectedDoc = function(docname){
               $scope.selectedDoc = docname;
               $scope.openDoc = null;
           };
 
+          /**
+           * Sets the StartDateFilter
+           * @param start
+           */
           $scope.setSelectedStartDate = function(start){
               $scope.selectedStart = start;
               $scope.getListFromServer();
               $scope.openstartTime = null;
           };
 
+          /**
+           * Sets the EndDateFilter
+           * @param end
+           */
           $scope.setSelectedEndDate = function(end){
               $scope.selectedEnd = end;
               $scope.getListFromServer();
               $scope.openendTime2 = null;
           };
 
+
+          /**
+           * Requests op slot list from server and adds from and to parameters to the requests
+           * Afterwards the list will be refreshed
+           */
           $scope.getListFromServer = function () {
               var params = "?";
               if($scope.selectedStart != null){
@@ -108,10 +130,8 @@ angular.module('myApp.listop', ['ngRoute'])
             var json = " { 'type' : '" + $scope.data.opType  + "', 'slotStart' : " + $scope.data.slotStart.getTime() + ", 'slotEnd' : " + $scope.data.slotEnd.getTime() + " }";
             $http.put('http://localhost:8080/opslots/create', json)
                 .success(function (data, status, headers, config) {
-
                 })
                 .error(function (data, status, headers, config) {
-
                 });
         };
     }]);
