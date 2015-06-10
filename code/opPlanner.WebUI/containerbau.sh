94efeb6146c0e23a1a2b5c35704b97400e5799ff
Lukas Kraenkl <studium@kraenkl.com>
2015-06-10 10:02:22 +0200

    Webserver Docker now starting

---
+++ code/opPlanner.WebUI/containerbau.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "remove previous container if exists..."
+docker rm -f webserver
+echo "rebuilt app Dockerfile"
+docker build -t webserver .
+docker run -d -p 80:80  --name webserver webserver
