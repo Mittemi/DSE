08c2866d5e18a39b7fee47bc2b0102411c8212b9
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-06-17 13:39:50 +0200

    gradle upgrad and fix

---
+++ code/opPlanner.Shared/src/main/java/opPlanner/Shared/Application.java
@@ -0,0 +1,14 @@
+package opPlanner.Shared;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Michael on 17.06.2015.
+ */
+public class Application {
+
+    // workaround for gradle!
+    public static void main(String[] args) {
+
+    }
+}
