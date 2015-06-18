c64b91d09558c6fa902f611e0204c9bf9c9bf26e
Lukas Kraenkl <lukas@kraenkl.com>
2015-06-18 02:17:20 +0200

    Port Forwarding

--- code/opPlanner.KLINIsys/containerbau.sh
+++ code/opPlanner.KLINIsys/containerbau.sh
@@ -10,16 +10,19 @@ pattern="s/127.0.0.1/$1/g"
 echo "Pattern: $pattern"
 cp src/main/resources/opPlanner.yml src/main/resources/opPlanner_tmp.yml
 cp src/main/resources/application.yml src/main/resources/application_tmp.yml
-
 cat src/main/resources/opPlanner.yml | \
-sed -e $pattern > src/main/resources/opPlanner.yml
+sed -e $pattern > tmp
+mv tmp src/main/resources/opPlanner.yml
+
+pattern2="s/127.0.0.1/$1/g"
 cat src/main/resources/application.yml | \
-sed -e $pattern > src/main/resources/application.yml
+sed -e $pattern2 > tmp
+mv tmp src/main/resources/application.yml
 echo "rebuilt app Dockerfile"
 gradle build -x test
 docker build -t klinisys .
 echo "HOST_IP=$1"
-docker run -d -p 9000:8080 --name klinisys klinisys
+docker run -d -p 9000:9000 --name klinisys klinisys
 rm src/main/resources/opPlanner.yml
 mv src/main/resources/opPlanner_tmp.yml src/main/resources/opPlanner.yml 
 rm src/main/resources/application.yml
