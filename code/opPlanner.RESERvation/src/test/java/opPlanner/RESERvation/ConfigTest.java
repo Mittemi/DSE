4514c66d5d39a161969ed2038a6cc6d60a33faec
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-06-19 11:14:22 +0200

    WebIntegrationTests for reservation

--- code/opPlanner.RESERvation/src/test/java/opPlanner/RESERvation/ConfigTest.java
+++ code/opPlanner.RESERvation/src/test/java/opPlanner/RESERvation/ConfigTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
@@ -13,6 +14,7 @@ import static org.junit.Assert.assertNotNull;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest({"server.port=0", "management.port=0"})
 public class ConfigTest {
 
     @Autowired
