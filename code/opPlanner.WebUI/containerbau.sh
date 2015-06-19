2490b55b47a05ea10ed95f836a19c41e925c4c16
Lukas Kraenkl <lukas@kraenkl.com>
2015-06-19 09:39:31 +0200

    Remove bower install from Containerbau.sh

--- code/opPlanner.WebUI/containerbau.sh
+++ code/opPlanner.WebUI/containerbau.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "remove previous container if exists..."
 docker rm -f webserver
-echo "NOW INSTALLING LATEST BOWER PACKAGES"
-bower install
 echo "rebuilt app Dockerfile"
 docker build -t webserver .
 docker run -d -p 80:80  --name webserver webserver
