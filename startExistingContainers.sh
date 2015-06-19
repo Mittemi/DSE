3975f4b00f8b6d137814ab7fac566ddbb350cac7
Michael Mittermayr <mmichael@edumail.at>
2015-06-19 18:05:32 +0200

    Start existing containers script changed

--- startExistingContainers.sh
+++ startExistingContainers.sh
@@ -1,8 +1,15 @@
 docker start mongo
 docker start postgres
-docker start apigateway
+echo "Wait a few seconds for docker and postgres"
+sleep 5
 docker start klinisys
+echo "Wait a few seconds for klinisys to start"
+sleep 20
+docker start apigateway
 docker start reservation
+echo "Initialize Demo Data for Klinisys"
+curl http://localhost:9000/demo
+echo -e "\n"
 docker start opmatcher
 docker start notifier
 
